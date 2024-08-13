c994160f60c418222161a0a43aef539bff8be06a
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-08-13 14:31:44 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,55 @@
+/*
+No = 5
+Pattern is:
+5
+4
+3
+2
+1 
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    public int No;
+
+    public Pattern(int iValue)
+    {
+        No = iValue;
+    }
+
+    public void DisplayPattern()
+    {
+        int iCnt = 0;
+
+        if(No < 0)
+        {
+            No =- No;
+        }
+
+        System.out.println("Pattern is:");
+        for(iCnt = No; iCnt >=1; iCnt--)
+        {
+            System.out.println(iCnt);
+        }
+    }
+
+}
+
+class Pattern4
+{
+    public static void main(String arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iNo = 0;
+
+        System.out.println("Enter the number to print reverse pattern till 1:");
+        iNo = sobj.nextInt();
+
+        Pattern pobj = new Pattern(iNo);
+
+        pobj.DisplayPattern();
+    }
+}
