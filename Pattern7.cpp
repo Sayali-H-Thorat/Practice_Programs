f94fe8bc93210213c9c7374a5e95fd15f720727f
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-08-15 13:18:40 +0530

    Add files via upload

---
+++ Pattern7.cpp
@@ -0,0 +1,51 @@
+/*
+No = 5
+Pattern is:
+A       B       C       D       E
+*/
+
+#include<iostream>
+using namespace std;
+
+class Pattern
+{
+    public:
+     int No;
+
+     Pattern(int iValue)
+     {
+        No = iValue;
+     }
+
+     void DisplayPattern()
+     {
+        int iCnt = 0;
+        char ch = 'A';
+
+        if(No < 0)
+        {
+            No = -No;
+        }
+
+        cout<<"Pattern is:\n";
+        for(iCnt = 1; iCnt <= No; iCnt++)
+        {
+            cout<<ch<<"\t";
+            ch++;
+        }
+     }
+};
+
+int main()
+{
+    int iNo = 0;
+
+    cout<<"Enter the number to print capital letters pattern:\n";
+    cin>>iNo;
+
+    Pattern pobj(iNo);
+
+    pobj.DisplayPattern();
+
+    return 0;
+}
