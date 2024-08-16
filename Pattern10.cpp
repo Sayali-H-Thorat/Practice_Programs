e1e29cbef445674812a21f0696f3be87a8ee1719
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-08-16 13:25:01 +0530

    Add files via upload

---
+++ Pattern10.cpp
@@ -0,0 +1,64 @@
+/*
+iRow = 4, iCol = 5
+1       2       3       4       5
+1       2       3       4       5
+1       2       3       4       5
+1       2       3       4       5
+*/
+
+#include<iostream>
+using namespace std;
+
+class Pattern
+{
+    public:
+     int iRow;
+     int iCol;
+
+     Pattern(int No1, int No2)
+     {
+        iRow = No1;
+        iCol = No2;
+     }
+
+     void DisplayPattern()
+     {
+        int i = 0;
+        int j = 0;
+
+        if(iRow < 0)
+        {
+            iRow = -iRow;
+        }
+        if(iCol < 0)
+        {
+            iCol = -iCol;
+        }
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                cout<<j<<"\t";
+            }
+            cout<<"\n";
+        }
+     }
+};
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    cout<<"Enter number of rows:\n";
+    cin>>iValue1;
+
+    cout<<"Enter number of columns:\n";
+    cin>>iValue2;
+
+    Pattern pobj(iValue1,iValue2);
+
+    pobj.DisplayPattern();
+
+    return 0;
+}
