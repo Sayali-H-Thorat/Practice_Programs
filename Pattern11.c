2630aa752eabe8eac7b37272134918eb9d68a5b8
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-08-17 11:56:45 +0530

    Add files via upload

---
+++ Pattern11.c
@@ -0,0 +1,51 @@
+/*
+iRow = 5, iCol = 5
+1       1       1       1       1
+2       2       2       2       2
+3       3       3       3       3
+4       4       4       4       4
+5       5       5       5       5
+*/
+#include<stdio.h>
+
+void DisplayPattern(int iRow,int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    if(iRow < 0)
+    {
+        iRow = -iRow;
+    }
+
+    if(iCol < 0)
+    {
+        iCol = -iCol;
+    }
+
+    printf("Pattern is:\n");
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j<= iCol; j++)
+        {
+            printf("%d\t",i);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of rows:\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns:\n");
+    scanf("%d",&iValue2);
+
+    DisplayPattern(iValue1,iValue2);
+
+    return 0;
+}
