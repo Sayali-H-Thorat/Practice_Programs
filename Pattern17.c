7bce08a3cd9e5231c0876976a9fb284283307f0c
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-08-20 17:59:01 +0530

    Add files via upload

---
+++ Pattern17.c
@@ -0,0 +1,60 @@
+/*
+iRow = 5, iCol = 5
+Pattern is:
+$       *       *       *       *
+*       $       *       *       *
+*       *       $       *       *
+*       *       *       $       *
+*       *       *       *       $
+*/
+
+#include<stdio.h>
+
+void DisplayPattern(int iRow,int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    if(iRow < 0)
+    {
+        iRow = -iRow;
+    }
+
+    if(iCol < 0)
+    {
+        iCol = -iCol;
+    }
+
+    printf("Pattern is:\n");
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == j)
+            {
+                printf("$\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of Rows:\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns:\n");
+    scanf("%d",&iValue2);
+
+    DisplayPattern(iValue1,iValue2);
+
+    return 0;
+}
