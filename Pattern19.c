3344151993622569c95e6abaf4c0507754433eb6
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-08-21 12:24:59 +0530

    Add files via upload

---
+++ Pattern19.c
@@ -0,0 +1,59 @@
+/*
+iRow = 4,iCol = 4
+Pattern is:
+@       2       3       4
+1       @       3       4
+1       2       @       4
+1       2       3       @
+*/
+
+#include<stdio.h>
+
+void DisplayPattern(int iRow,int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    if(iRow < 0)
+    {
+        iRow = -iRow;
+    }
+
+    if(iCol < 0)
+    {
+        iCol = -iCol;
+    }
+
+    printf("Pattern is:\n");
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == j)
+            {
+                printf("@\t");
+            }
+            else
+            {
+                printf("%d\t",j);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of rows:\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns:\n");
+    scanf("%d",&iValue2);
+
+    DisplayPattern(iValue1,iValue2);
+
+    return 0;
+}
