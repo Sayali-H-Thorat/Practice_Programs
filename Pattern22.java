f35be411510c0e09af7b43f26fb1e4226d037eea
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-08-22 19:51:49 +0530

    Add files via upload

---
+++ Pattern22.java
@@ -0,0 +1,77 @@
+/*
+iRow = 5, iCol = 5
+Pattern is:
+&       $       $       $       $
+*       &       $       $       $
+*       *       &       $       $
+*       *       *       &       $
+*       *       *       *       &
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    public int iRow;
+    public int iCol;
+
+    public Pattern(int No1,int No2)
+    {
+        iRow = No1;
+        iCol = No2;
+    }
+
+    public void DisplayPattern()
+    {
+        int i = 0;
+        int j = 0;
+
+        if(iRow != iCol)
+        {
+            System.out.println("Number of rows and columns are differrent.");
+            return;
+        }
+
+        System.out.println("Pattern is:");
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(i == j)
+                {
+                    System.out.print("&\t");
+                }
+                else if( i < j)
+                {
+                    System.out.print("$\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Pattern22
+{
+    public static void main(String arg[])
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of rows:");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns:");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern(iValue1,iValue2);
+
+        pobj.DisplayPattern();
+    }
+}
