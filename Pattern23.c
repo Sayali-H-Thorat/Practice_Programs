40b4d707814628a450f75e4dc10f813be91f94cb
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-08-23 19:15:48 +0530

    Add files via upload

---
+++ Pattern23.c
@@ -0,0 +1,56 @@
+/*
+iRow = 4,iCol = 4
+Pattern is:
+#       1       1       1
+2       #       2       2
+3       3       #       3
+4       4       4       #
+*/
+
+#include<stdio.h>
+
+void DisplayPattern(int iRow,int iCol)
+{
+    int i = 0;
+    int j = 0;
+
+    if(iRow != iCol)
+    {
+        printf("Number of rows and columns are differrent.\n");
+        return;
+    }
+
+    printf("Pattern is:\n");
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == j)
+            {
+                printf("#\t");
+            }
+            else
+            {
+                printf("%d\t",i);
+            }
+        }
+        printf("\n");
+    }
+}
+
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter number of rows:\n");
+    scanf("%d",&iValue1);
+
+    printf("Enter number of columns:\n");
+    scanf("%d",&iValue2);
+
+    DisplayPattern(iValue1,iValue2);
+
+    return 0;
+}
