0f2748f9f11e9193b5569a717c17b59ae254e4d5
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-08-29 13:44:09 +0530

    Add files via upload

---
+++ Pattern34.cpp
@@ -0,0 +1,76 @@
+/*
+iRow = 4, iCol = 4
+Pattern is:
+                        *
+                *       *       *
+        *       *       *       *       *
+*       *       *       *       *       *       *
+*/
+
+#include<iostream>
+using namespace std;
+
+class Pattern 
+{
+    public:
+     int iRow;
+     int iCol;
+
+     Pattern(int No1,int No2)
+     {
+        iRow = No1;
+        iCol = No2;
+     }
+
+     void DisplayPattern()
+     {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        if(iRow != iCol)
+        {
+            cout<<"Number of rows and columns are differrent.\n";
+            return;
+        }
+
+        cout<<"Pattern is:\n";
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if(j <= iRow - i)
+                {
+                    cout<<" \t";
+                }
+                else
+                {
+                    cout<<"*\t";
+                }
+            }
+            for(k = 1; k < i; k++)
+            {
+                cout<<"*\t";
+            }
+            cout<<"\n";
+        }
+     }
+};
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    cout<<"Enter number of rows:\n";
+    cin>>iValue1;
+
+    cout<<"Enter number of columns:\n";
+    cin>>iValue2;
+
+    Pattern pobj(iValue1,iValue2);
+
+    pobj.DisplayPattern();
+
+    return 0;
+}
