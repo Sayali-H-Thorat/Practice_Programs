88ac2a9bce74e014a8e6bd0a3c8361be39f0ffcf
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-08-30 13:41:52 +0530

    Add files via upload

---
+++ Pattern36.java
@@ -0,0 +1,69 @@
+/*
+iRow = 4, iCol = 4
+Pattern is:
+****
+ ****
+  ****
+   ****
+*/
+import java.util.*;
+
+class Pattern
+{
+    public int iRow;
+    public int iCol;
+
+    public Pattern(int No1,int No2)
+    {
+        iRow = No1;
+        iCol = No2;
+    }
+
+    public void DisplayPattern()
+    {
+        int i = 0;
+        int j = 0;
+
+        if(iRow != iCol)
+        {
+            System.out.println("Number of rows and columns are differrent.");
+            return;
+        }
+
+        System.out.println("Pattern is:");
+        for(i = iRow; i >= 1; i--)
+        {
+            for(j = 1; j <= iRow - i; j++)
+            {
+                System.out.print(" ");
+            }
+
+            for(j = 1; j <= iCol; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Pattern36
+{
+    public static void main(String arg[])
+    {
+        int iValue1 = 0;
+        int iValue2 = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter number of rows:");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns:");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern(iValue1,iValue2);
+
+        pobj.DisplayPattern();
+    }
+}
