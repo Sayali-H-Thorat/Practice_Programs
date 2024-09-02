2b5cd663957503f911ef1b979f31c3e12abdf155
Sayali <119116326+Sayali-H-Thorat@users.noreply.github.com>
2024-09-02 14:35:17 +0530

    Add files via upload

---
+++ Pattern38.cpp
@@ -0,0 +1,76 @@
+/*
+iRow = 10, iCol = 10
+Pattern is:
+*
+**
+* *
+*  *
+*   *
+*    *
+*     *
+*      *
+*       *
+**********
+*/
+#include<iostream>
+using namespace std;
+
+class Pattern
+{
+    public:
+     int iRow;
+     int iCol;
+
+     Pattern(int No1,int No2)
+     {
+        iRow = No1;
+        iCol = No2;
+     }
+
+     void DisplayPattern()
+     {
+        int i = 0;
+        int j = 0;
+
+        if(iRow != iCol)
+        {
+            cout<<"Number of rows and columns are differrent.\n";
+            return;
+        }
+
+        cout<<"Pattern is:\n";
+        for(i = 1; i<= iRow; i++)
+        {
+            for(j = 1; j <= iCol; j++)
+            {
+                if((j == 1)||(i == j)||(i == iRow))
+                {
+                    cout<<"*";
+                }
+                else
+                {
+                    cout<<" ";
+                }
+            }
+            cout<<"\n";
+        }
+     }
+};
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    cout<<"Enter number of rows:\n";
+    cin>>iValue1;
+
+    cout<<"Enter number of columns:\n";
+    cin>>iValue2;
+
+    Pattern pobj(iValue1,iValue2);
+
+    pobj.DisplayPattern();
+
+    return 0;
+}
